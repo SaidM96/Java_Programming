353e8a860ff250300ddd32565a04a6b324704600
Said Mia <saiid.revolution@gmail.com>
2023-07-24 10:55:36 +0100

    finish ex

---
+++ module_01/ex01/Program.java
@@ -0,0 +1,16 @@
+package ex01;
+
+public class Program {
+    public static void main(String[] args){
+        User userA = new User("Said");
+        User userB = new User("anas");
+        User userC = new User("ahmed");
+
+        System.out.println(userA.getName() + " id: " + userA.getId());
+        System.out.println(userB.getName() + " id: " + userB.getId());
+        System.out.println(userC.getName() + " id: " + userC.getId());
+    }
+}
+
+// to compile : javac ex01/Program.java  ex01/UserIdsGenerator.java ex01/User.java
+// to execute : java ex01/Program
---
+++ module_01/ex01/User.java
@@ -0,0 +1,20 @@
+package ex01;
+
+public class User {
+    private int id;
+    private String name;
+
+    public User(String name){
+        this.id = UserIdsGenerator.getInstance().generateId();
+        this.name = name;
+    }
+
+    public int getId(){
+        return this.id;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+}
