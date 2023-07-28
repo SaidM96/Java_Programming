1be24767977244680f3c447bb50a7bd355f44626
Said Mia <saiid.revolution@gmail.com>
2023-07-28 18:27:02 +0100

    start problems in ex05 m00

--- module_00/ex00/Program.java
+++ module_00/ex00/Program.java
@@ -1,11 +1,25 @@
 public class Program {
   public static void main(String[] args) {
-    int number = 11122;
+    int number = 479598;
     int result = 0;
-    while(number != 0){
-        result += number % 10;
-        number /= 10;
-    }
+
+    result += number % 10;
+    number /= 10;
+
+    result += number % 10;
+    number /= 10;
+
+    result += number % 10;
+    number /= 10;
+
+    result += number % 10;
+    number /= 10;
+
+    result += number % 10;
+    number /= 10;
+
+    result += number % 10;
+    number /= 10;
     System.out.println(result);
   }
 }
