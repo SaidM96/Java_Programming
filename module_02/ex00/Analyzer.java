6ae4b893d1c1b8fbd4450e268a64182c63a8a03f
Said Mia <saiid.revolution@gmail.com>
2023-08-01 04:55:22 +0100

    finish ex00 m2

--- module_02/ex00/Analyzer.java
+++ module_02/ex00/Analyzer.java
@@ -1,18 +1,99 @@
 package ex00;
 import java.util.Map;
 import java.util.HashMap;
-
+import java.util.ArrayList;
+import java.util.List;
+import java.io.*;
 public class Analyzer {
-    private String path;
     private Map<String, String> fileSignatures; // signature key , fileEXtention value
-    
-    private fillMap(){
-        
+    private List<String> result;
+    private void fillMap(){
+        try{
+            // Step 1: Open the file for reading
+            FileInputStream fis = new FileInputStream("/goinfre/smia/Java_Programming/module_02/ex00/signatures.txt");
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader br = new BufferedReader(isr);
+            String line = new String("");
+            String[] params = new String[2];
+            while((line = br.readLine()) != null){
+                params = line.split(",");
+                String signature = params[1].replace(" ","");
+                fileSignatures.put(signature, signature.length() + "!" + params[0]);
+            }
+        }
+        catch(Exception e){
+            //
+        }
     }
     
-    public Analyzer(String path){
-        this.path = path;
+    public Analyzer(){
         this.fileSignatures = new HashMap<>();
+        this.result = new ArrayList<>();
         this.fillMap();
     }
+
+    private boolean signatureChecker(String sign){
+        String result = new String("");
+        for (Map.Entry<String, String> entry : fileSignatures.entrySet()) {
+                String fileExt = entry.getValue();
+                String[] params = fileExt.split("!");
+                String Mysignature = entry.getKey().substring(0,Integer.parseInt(params[0]));
+                if (Mysignature.equals(sign.substring(0, Mysignature.length()))){
+                    this.result.add(params[1]);
+                    System.out.println("PROCESSED");
+                    return true;
+                }
+            }
+        return false;
+    }
+
+    public boolean checkPath(String path){
+        try{
+            InputStream inputStream = new FileInputStream(path);
+            byte[] buffer = new byte[8]; // Buffer to hold the first 8 bytes
+
+            int bytesRead = inputStream.read(buffer);
+            if (bytesRead == 8) {
+                StringBuilder hexSignature = new StringBuilder();
+                for (byte b : buffer) {
+                    hexSignature.append(String.format("%02X", b));
+                }
+                String signature = hexSignature.toString();
+                if (!this.signatureChecker(signature)){
+                    this.result.add("UNDEFINED");
+                    System.out.println("UNDEFINED");
+                }
+                else
+                    return true;
+            }
+            return false;
+        }
+        catch(Exception e){
+            System.out.print(e);
+            return false;
+        }
+    }
+
+    private String resultToString(){
+        String result = new String("");
+        for (String file : this.result) {
+            result += file + "\n";
+        }
+        return result;
+    }
+
+
+    public void generateResultFile(){
+        String fileName = "result.txt"; // Replace this with your desired file name
+        String content = this.resultToString();
+
+        try (FileOutputStream fos = new FileOutputStream(fileName)) {
+            byte[] bytes = content.getBytes();
+            fos.write(bytes);
+            System.out.println("File created and data written successfully.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
--- module_02/ex00/Program.java
+++ module_02/ex00/Program.java
@@ -3,9 +3,16 @@ import java.util.Scanner;
 
 public class Program{
     public static void main(String[] args){
+        Analyzer analyzer = new Analyzer();
         Scanner in = new Scanner(System.in);
-        String line = in.nextLine();
-        Analyzer analyzer = new Analyzer(line);
-        
+        while(true){
+            String line = in.nextLine();
+            if (line.length() == 2 && line.indexOf("42") == 0){
+                analyzer.generateResultFile();
+                break ;
+            }
+            if (!analyzer.checkPath(line))
+                break ;
+        }
     }
 }
