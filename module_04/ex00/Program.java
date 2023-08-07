d6bb24dc83268a589c378e968a23a488c3fd07ac
Said Mia <saiid.revolution@gmail.com>
2023-08-07 03:24:18 +0100

    finish e00 day4

---
+++ module_04/ex00/Program.java
@@ -0,0 +1,13 @@
+package ex00;
+
+public class Program {
+    public static void main(String[] args){
+        String bg = args[0];
+        String it = args[1];
+
+        ReadBMPImage readerImage = new ReadBMPImage();
+        int[][] pixels = readerImage.getPixelColors();
+        Printer printer = new Printer(bg,it,pixels);
+        printer.printIt();
+    }
+}
---
+++ module_04/ex00/ReadBMPImage.java
@@ -0,0 +1,38 @@
+package ex00;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+public class ReadBMPImage {
+    
+    public ReadBMPImage(){
+
+    }
+
+    public int[][] getPixelColors(){
+            int[][] pixelColors = new int[16][16];
+            try {
+                // Load the BMP image
+                File imageFile = new File("/goinfre/smia/zbi/module_04/ex00/it.bmp");
+                BufferedImage image = ImageIO.read(imageFile);
+                // Get image dimensions
+                int width = 16;
+                int height = 16;
+                // Create a 2D array to store the RGB values of each pixel
+                for (int y = 0; y < height; y++) {
+                    for (int x = 0; x < width; x++) {
+                        // Get the RGB value of the pixel at (x, y)
+                        int rgb = image.getRGB(x, y);
+                        pixelColors[y][x] = rgb;
+                    }
+                }
+            } 
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return pixelColors;
+    }
+    
+}
