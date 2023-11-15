e04c4d18abb1e3d03fb4d9b9aec2dc71f8e51752
said <saiid.revolution@gmail.com>
2023-11-15 22:54:02 +0100

    finish Day

--- module_02/ex02/Program.java
+++ module_02/ex02/Program.java
@@ -6,7 +6,10 @@ public class Program{
             System.out.println("Please provide exactly full path of current dir.");
             return ;
         }
+        if (!args[0].contains("--current-folder=")){
+            return ;
+        }
         FileManager program = new FileManager();
-        program.startProgram(args[0]);
+        program.startProgram(args[0].replace("--current-folder=", ""));
     }
 }
